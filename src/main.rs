cc4a269aac35103b9dfc7e5afacd8ad016eadb25
Rafael Fernández López <ereslibre@ereslibre.es>
2025-03-11 15:19:53 +0100

    feature: correct by construction

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,13 @@ static A: AllocDisabler = AllocDisabler;
 mod roman;
 
 fn main() {
-    let cases = [
-        "I", "II", "III", "IV", "VI", "XIX", "XX", "XXI", "MCMXIX", "XXIJK",
-    ];
+    let cases = ["I", "II", "III", "IV", "VI", "XIX", "XX", "XXI", "MCMXIX"];
     for case in cases {
-        let decimal = assert_no_alloc(|| roman::roman_to_decimal(case));
-        if let Ok(decimal) = decimal {
-            println!("Decimal number for {} is {:?}", case, decimal);
-        } else {
-            println!("Could not convert roman number {} to decimal", case);
-        };
+        let roman_case = case.chars().fold(Vec::new(), |mut acc, roman| {
+            acc.push(roman.try_into().unwrap());
+            acc
+        });
+        let decimal = assert_no_alloc(|| roman::roman_to_decimal(&roman_case));
+        println!("Decimal number for {} is {:?}", case, decimal);
     }
 }
--- src/roman.rs
+++ src/roman.rs
@@ -1,36 +1,57 @@
-struct RomanNumeral(char);
+pub enum RomanNumeral {
+    I,
+    V,
+    X,
+    L,
+    C,
+    D,
+    M,
+}
 
-impl TryFrom<RomanNumeral> for u16 {
+impl TryFrom<char> for RomanNumeral {
     type Error = &'static str;
 
-    #[inline]
-    fn try_from(numeral: RomanNumeral) -> Result<Self, Self::Error> {
+    fn try_from(numeral: char) -> Result<Self, Self::Error> {
         Ok(match numeral {
-            RomanNumeral('I') => 1,
-            RomanNumeral('V') => 5,
-            RomanNumeral('X') => 10,
-            RomanNumeral('L') => 50,
-            RomanNumeral('C') => 100,
-            RomanNumeral('D') => 500,
-            RomanNumeral('M') => 1000,
+            'I' => RomanNumeral::I,
+            'V' => RomanNumeral::V,
+            'X' => RomanNumeral::X,
+            'L' => RomanNumeral::L,
+            'C' => RomanNumeral::C,
+            'D' => RomanNumeral::D,
+            'M' => RomanNumeral::M,
             _ => return Err("Invalid roman numeral"),
         })
     }
 }
 
+impl From<&RomanNumeral> for u16 {
+    fn from(numeral: &RomanNumeral) -> Self {
+        match numeral {
+            RomanNumeral::I => 1,
+            RomanNumeral::V => 5,
+            RomanNumeral::X => 10,
+            RomanNumeral::L => 50,
+            RomanNumeral::C => 100,
+            RomanNumeral::D => 500,
+            RomanNumeral::M => 1000,
+        }
+    }
+}
+
 /// Calculates the decimal number corresponding to a given valid roman
 /// number.
-pub fn roman_to_decimal(roman: &str) -> Result<u16, &'static str> {
-    Ok(roman
-        .chars()
+pub fn roman_to_decimal(roman: &[RomanNumeral]) -> u16 {
+    roman
+        .iter()
         .rev()
-        .try_fold((0, 0), |(t, s), numeral| {
-            let numeral = RomanNumeral(numeral).try_into()?;
-            Ok(if numeral >= s {
+        .fold((0, 0), |(t, s), numeral| {
+            let numeral = numeral.into();
+            if numeral >= s {
                 (t + numeral, numeral)
             } else {
                 (t - numeral, numeral)
-            })
-        })?
-        .0)
+            }
+        })
+        .0
 }
